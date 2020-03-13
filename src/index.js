517d04b4b4c844d83a48032bd208e5c0ea60c609
jonathanengelbert <jonathanengelbert@gmail.com>
2020-03-13 12:22:55 -0400

    development

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import _ from 'lodash';
 import printMe from './print.js';
 
-console.log('jon');
-
 function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', '!'], ' ');
 
   btn.innerHTML = 'Click me and check the console';
   btn.onclick = printMe;
-  btn.onclick = () => console.log('stuff');
 
   element.appendChild(btn);
 
   return element;
 }
+
 
 document.body.appendChild(component());
