7bc3e5924d37066a6a880a517641c65174d05000
jonathanengelbert <jonathanengelbert@gmail.com>
2020-03-13 11:23:39 -0400

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 console.log('jon');
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
+  btn.onclick = () => console.log('stuff');
+
+  element.appendChild(btn);
+
   return element;
 }
 
